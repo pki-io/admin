9098be0c586b1c0ccbfec6044543a325920c20ea
thanasisk <platsakos@gmail.com>
2014-11-15 18:43:44 +0100

    added small help screen

--- admin-cli.go
+++ admin-cli.go
@@ -6,5 +6,13 @@ import (
     "github.com/docopt/docopt-go"
 )
 func main() {
+    usage := `
+Usage:
+    TBC
+Options:
+    -h --help   Show this screen
+    `
 
+   arguments, _ := docopt.Parse(usage, nil, true, "pki.io admin-CLI", false)
+   fmt.Println(arguments)
 }
