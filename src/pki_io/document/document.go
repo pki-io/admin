baf5876ebe826d6b6f563feb9fd862f11f6b9975
Fraser Scott <fraser.scott@gmail.com>
2014-11-21 00:36:07 +0000

    validation and creating json

--- src/pki_io/document/document.go
+++ src/pki_io/document/document.go
@@ -2,14 +2,9 @@ package document
 
 import (
     "fmt"
+    "strings"
     "encoding/json"
 )
-type Documenter interface {
-    Version()
-    Type()
-    Options()
-    Body()
-}
 
 type document struct {
     Version int
@@ -18,16 +13,30 @@ type document struct {
     Body interface{}
 }
 
+func (doc *document) parseJson(data interface{}) (error) {
+    var json_data []byte
+    switch t := data.(type) {
+        case []byte:
+            json_data = data.([]byte)
+        case string:
+            json_data = []byte(data.(string))
+        case nil:
+            json_data = []byte(`{"version":1,"type":"ca-document","options":{"source":"","signature-mode":""},"body":{}}`)
+        default:
+            return fmt.Errorf("Invalid input type: %T", t)
+    }
+    return json.Unmarshal(json_data, doc)
+}
+
 type Document struct {
     document
 }
 
-func New(json_data string) (*Document, error) {
+func New(data string) (*Document, error) {
     d := document{}
-    if err := json.Unmarshal([]byte(json_data), &d); err != nil {
+    if err := d.parseJson(data); err != nil {
         return nil, err
     }
-    //doc := Document{d}
     return &Document{d}, nil
 }
 
@@ -53,48 +62,49 @@ type CADocument struct {
 
 func NewCA(data interface{}) (*CADocument, error) {
     d := document{}
-
-    var json_data []byte
-    switch t := data.(type) {
-        case []byte:
-            json_data = data.([]byte)
-        case string:
-            json_data = []byte(data.(string))
-        case nil:
-            json_data = []byte(`{"version":1,"type":"ca-document","options":{"source":"","signature-mode":""},"body":{}}`)
-        default:
-            return nil, fmt.Errorf("Invalid input type: %T", t)
-    }
-
-    if err := json.Unmarshal(json_data, &d); err != nil {
+    if err := d.parseJson(data); err != nil {
         return nil, err
     }
 
-    if t := d.Type; t != "ca-document" {
-      return nil, fmt.Errorf("Invalid type for CADocument: %s", t)
+    doc := CADocument{d}
+    if err := doc.Validate(); err != nil {
+        return nil, err
+    } else {
+        return &doc, nil
+    }
+}
+
+func (doc *CADocument) Validate() (err error) {
+    var errors []string
+
+    if t := doc.document.Type; t != "ca-document" {
+      errors = append(errors, fmt.Sprintf("Invalid type for CADocument: %s", t))
     }
 
-    switch t := d.Options.(type) {
-      case map[string]interface{}:
-      default:
-      return nil, fmt.Errorf("Invalid options type. Must be a hash: %T", t)
+    switch t := doc.document.Options.(type) {
+        case map[string]interface{}:
+        default:
+            errors = append(errors, fmt.Sprintf("Invalid options type. Must be a map: %T", t))
     }
 
-    if _, ok := d.Options.(map[string]interface{})["source"]; ! ok {
-      return nil, fmt.Errorf("Options source missing")
+    if _, ok := doc.document.Options.(map[string]interface{})["source"]; ! ok {
+        errors = append(errors, fmt.Sprintf("Options source missing"))
     }
 
-    if _, ok := d.Options.(map[string]interface{})["signature-mode"]; ! ok {
-      return nil, fmt.Errorf("Options signature-mode missing")
+    if _, ok := doc.document.Options.(map[string]interface{})["signature-mode"]; ! ok {
+        errors = append(errors, fmt.Sprintf("Options signature-mode missing"))
     }
 
-    switch t := d.Body.(type) {
-      case map[string]interface{}:
-      default:
-      return nil, fmt.Errorf("Invalid body type. Must be a hash: %T", t)
+    switch t := doc.document.Body.(type) {
+        case map[string]interface{}:
+        default:
+            errors = append(errors, fmt.Sprintf("Invalid body type. Must be a hash: %T", t))
     }
 
-    return &CADocument{d}, nil
+    if len(errors) > 0 {
+        err = fmt.Errorf("Could not validate: %s", strings.Join(errors, ", "))
+    }
+    return
 }
 
 func (doc *CADocument) Version() int {
@@ -111,4 +121,19 @@ func (doc *CADocument) Options() map[string]interface{} {
 
 func (doc *CADocument) Body() map[string]interface{} {
     return doc.document.Body.(map[string]interface{})
+}
+
+func (doc *CADocument) Json() (string, error) {
+    if err := doc.Validate(); err != nil {
+        return "", err
+    } else if b, err := json.Marshal(doc.document); err != nil {
+        return "", err
+    } else {
+        return string(b[:]), nil
+    }
+}
+
+func (doc *CADocument) String() string {
+    str, _ := doc.Json()
+    return str
 }
