c437532a1caee032dc84f199e46b8b595e9c7de9
Fraser Scott <fraser.scott@gmail.com>
2014-11-26 00:23:55 +0000

    json schema and structs finally working together

--- src/pki_io/document/document.go
+++ src/pki_io/document/document.go
@@ -1,139 +1,58 @@
 package document
 
 import (
-    "fmt"
-    "strings"
     "encoding/json"
+    "fmt"
+    "github.com/xeipuuv/gojsonschema"
 )
 
 type document struct {
-    Version int
-    Type string
-    Options interface{}
-    Body interface{}
+    schema string
+    default_value string
 }
 
-func (doc *document) parseJson(data interface{}) (error) {
-    var json_data []byte
+func (doc *document) parseJson(data interface{}, target interface{}) (interface{}, error) {
+    var jsonData []byte
     switch t := data.(type) {
-        case []byte:
-            json_data = data.([]byte)
-        case string:
-            json_data = []byte(data.(string))
-        case nil:
-            json_data = []byte(`{"version":1,"type":"ca-document","options":{"source":"","signature-mode":""},"body":{}}`)
-        default:
-            return fmt.Errorf("Invalid input type: %T", t)
+    case []byte:
+        jsonData = data.([]byte)
+    case string:
+        jsonData = []byte(data.(string))
+    case nil:
+        jsonData = []byte(doc.default_value)
+    default:
+        return nil, fmt.Errorf("Invalid input type: %T", t)
     }
-    return json.Unmarshal(json_data, doc)
-}
 
-type Document struct {
-    document
-}
-
-func New(data string) (*Document, error) {
-    d := document{}
-    if err := d.parseJson(data); err != nil {
-        return nil, err
-    }
-    return &Document{d}, nil
-}
-
-func (doc *Document) Version() int {
-    return doc.document.Version
-}
-
-func (doc *Document) Type() string {
-    return doc.document.Type
-}
-
-func (doc *Document) Options() interface{} {
-    return doc.document.Options
-}
-
-func (doc *Document) Body() interface{} {
-    return doc.document.Body
-}
-
-type CADocument struct {
-    document
-}
-
-func NewCA(data interface{}) (*CADocument, error) {
-    d := document{}
-    if err := d.parseJson(data); err != nil {
+    var jsonDocument interface{}
+    if err := json.Unmarshal(jsonData, &jsonDocument); err != nil {
         return nil, err
     }
 
-    doc := CADocument{d}
-    if err := doc.Validate(); err != nil {
-        return nil, err
+    var schemaMap map[string]interface{}
+    if err := json.Unmarshal([]byte(doc.schema), &schemaMap); err != nil {
+        return nil, fmt.Errorf("Can't unmarshal schema: %s", err.Error())
+    }
+
+    schemaDocument, err := gojsonschema.NewJsonSchemaDocument(schemaMap)
+    if err != nil {
+        return nil, fmt.Errorf("Can't create schema document: %s", err.Error())
+    }
+
+    result := schemaDocument.Validate(jsonDocument)
+    if result.Valid() {
+        fmt.Printf("The document is valid\n")
+        if err := json.Unmarshal(jsonData, target); err != nil {
+            return nil, err
+        } else {
+          return target, nil
+        }
     } else {
-        return &doc, nil
+        fmt.Printf("The document is not valid. see errors :\n")
+        // Loop through errors
+        for _, desc := range result.Errors() {
+            fmt.Printf("- %s\n", desc)
+        }
+        return nil, fmt.Errorf("ffs")
     }
-}
-
-func (doc *CADocument) Validate() (err error) {
-    var errors []string
-
-    if t := doc.document.Type; t != "ca-document" {
-      errors = append(errors, fmt.Sprintf("Invalid type for CADocument: %s", t))
-    }
-
-    switch t := doc.document.Options.(type) {
-        case map[string]interface{}:
-        default:
-            errors = append(errors, fmt.Sprintf("Invalid options type. Must be a map: %T", t))
-    }
-
-    if _, ok := doc.document.Options.(map[string]interface{})["source"]; ! ok {
-        errors = append(errors, fmt.Sprintf("Options source missing"))
-    }
-
-    if _, ok := doc.document.Options.(map[string]interface{})["signature-mode"]; ! ok {
-        errors = append(errors, fmt.Sprintf("Options signature-mode missing"))
-    }
-
-    switch t := doc.document.Body.(type) {
-        case map[string]interface{}:
-        default:
-            errors = append(errors, fmt.Sprintf("Invalid body type. Must be a hash: %T", t))
-    }
-
-    if len(errors) > 0 {
-        err = fmt.Errorf("Could not validate: %s", strings.Join(errors, ", "))
-    }
-    return
-}
-
-func (doc *CADocument) Version() int {
-    return doc.document.Version
-}
-
-func (doc *CADocument) Type() string {
-    return doc.document.Type
-}
-
-func (doc *CADocument) Options() map[string]interface{} {
-    return doc.document.Options.(map[string]interface{})
-}
-
-func (doc *CADocument) Body() map[string]interface{} {
-    return doc.document.Body.(map[string]interface{})
-}
-
-func (doc *CADocument) Json() (string, error) {
-    if err := doc.Validate(); err != nil {
-        return "", err
-    } else if b, err := json.Marshal(doc.document); err != nil {
-        return "", err
-    } else {
-        return string(b[:]), nil
-    }
-}
-
-func (doc *CADocument) String() string {
-    str, _ := doc.Json()
-    return str
 }
--- src/pki_io/main.go
+++ src/pki_io/main.go
@@ -6,22 +6,11 @@ import (
 )
 
 func main() {
-    if doc, err := document.New(`{}`); err != nil {
-        fmt.Println(err)
-    } else {
-        fmt.Println(doc)
-    }
-
-    if doc, err := document.NewCA(`{"version":1,"type":"ca-document","options":{"source":"123","signature-mode":""},"body":{}}`); err != nil {
-        fmt.Println(err)
-    } else {
-        fmt.Println(doc)
-    }
-
     if doc, err := document.NewCA(nil); err != nil {
         fmt.Println(err)
     } else {
-        doc.Options()["source"] = "333"
-        fmt.Println(doc)
+        fmt.Println("Source is: ", doc.Data.Options.Source)
+        doc.Data.Options.Source = "aabbccddeeff00112233445566778899"
+        fmt.Println("Source is now: ", doc.Data.Options.Source)
     }
 }
